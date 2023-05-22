01a95cf4b00aa9f80740f14ad8b4dba12e4ce3f9
Ben Noon <Ben@Nooneliterecruitment.com>
2023-05-22 11:49:22 +0100

    Added custom home template

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Template Name: Home
+*/
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+<p>This i what we wanna see!</p>
+		
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
